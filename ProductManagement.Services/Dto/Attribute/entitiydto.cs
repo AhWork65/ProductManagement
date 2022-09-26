e7e381d8fdea9e45f86df9ff7762aba00cb628ac
AhWork65 <ah.work65@gmail.com>
2022-09-26 13:06:00 +0330

    Create entitiydto.cs

---
+++ ProductManagement.Services/Dto/Attribute/entitiydto.cs
@@ -0,0 +1,23 @@
+﻿
+
+using ProductManagementDomain.Models.BaseEntities;
+
+namespace ProductManagement.Services.Dto.Attribute
+{
+    public class AttributeDto:DomainEntity
+    {
+        public AttributeDto()
+        {
+
+            Id = -1;
+            ParentId = -1;
+        }
+
+    public string Name { get; set; }
+    public string Value { get; set; }
+   
+
+    public int? ParentId { get; set; }
+
+    }
+}
